fe1a2f1e6b8ce4e664baa4e2056950b55e5ff0b4
Isabelle de Boer <Larsdeboer@quicknet.nl>
2025-04-11 18:54:12 +0200

    Documented ray class

--- src/ray_math.rs
+++ src/ray_math.rs
@@ -1,5 +1,18 @@
 use crate::vec_math::Vec3;
 
+/// Struct for representing mathmatical rays, consisting of an origin and a direction.
+/// # Example
+/// ```
+/// use renders::{ray_math::*, vec_math::*};
+/// let origin = Vec3::new(0.0, 1.0, 0.0);
+/// let direction = Vec3::new(0.0, -2.0, 0.0);
+/// 
+/// let ray = Ray::new(origin, direction);
+/// assert_ne!(*ray.direction(), direction);
+/// assert_eq!(*ray.direction(), Vec3::new(0.0, -1.0, 0.0)); // Ray direction is always of unit length.
+/// 
+/// assert_eq!(ray.at(1.0), Vec3::new(0.0, 0.0, 0.0));
+/// ```
 #[derive(Debug, PartialEq)]
 pub struct Ray {
     origin: Vec3,
@@ -7,6 +20,8 @@ pub struct Ray {
 }
 
 impl Ray {
+    /// Creates a new ray with origin `origin` and direction `direction.normalized()`.
+    #[must_use]
     pub fn new(origin: Vec3, direction: Vec3) -> Self {
         Self {
             origin,
@@ -14,15 +29,29 @@ impl Ray {
         }
     }
 
+    /// Returns the point t distance along the ray.
+    /// ```
+    /// use renders::{ray_math::*, vec_math::*};
+    /// let example = Ray::new(
+    /// Vec3::new(0.0, 1.0, 0.0),
+    /// Vec3::new(1.0, 0.0, 0.0)
+    /// );
+    /// assert_eq!(example.at(4.0), Vec3::new(4.0, 1.0, 0.0));
+    /// ```
+    #[must_use]
     pub fn at(&self, t:f64) -> Vec3 {
         *self.origin() + *self.direction() * t
     }
 
-    pub fn origin(&self) -> &Vec3 {
+    /// Returns an immutable borrow from the origin.
+    #[must_use]
+    pub const fn origin(&self) -> &Vec3 {
         &self.origin
     }
 
-    pub fn direction(&self) -> &Vec3 {
+    /// Returns an immutable borrow from the direction.
+    #[must_use]
+    pub const fn direction(&self) -> &Vec3 {
         &self.direction
     }
 }
@@ -37,8 +66,8 @@ mod tests {
         let direction = Vec3::new(2.0, 0.0, 0.0);
 
         let ray = Ray::new(origin, direction);
-        let point = ray.at(2.0);
-        assert_eq!(point, Vec3::new(2.0, 0.0, 0.0));
+        let point = ray.at(-2.0);
+        assert_eq!(point, Vec3::new(-2.0, 0.0, 0.0));
         assert_eq!(origin, *ray.origin());
         assert_eq!(direction.normalized(), *ray.direction());
     }
