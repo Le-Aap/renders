f31084e658183b49cca0102ef98aff69f210da6e
Isabelle de Boer <larsdeboer@quicknet.nl>
2025-08-08 20:14:58 +0200

    made a new scene that also shows reflection

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,20 @@ use renders::{brdfs::{self, BRDF}, camera::CameraBuilder, colors::Color, vec_mat
 fn main() {
     let mut world = Hittables::new();
 
-    let diffuse_grey_material: BRDF = brdfs::make_lambertian_diffuse_brdf(Color::new(0.6, 0.6, 0.6));
-    let diffuse_red_material: BRDF  = brdfs::make_lambertian_diffuse_brdf(Color::new(0.9, 0.3, 0.3));
+    let ground_material: BRDF = brdfs::make_lambertian_diffuse_brdf(Color::new(0.8, 0.8, 0.0));
+    let center_material: BRDF  = brdfs::make_lambertian_diffuse_brdf(Color::new(0.1, 0.2, 0.5));
+    let left_material: BRDF = brdfs::make_metal_brdf(Color::new(0.8, 0.8, 0.8));
+    let right_material: BRDF = brdfs::make_metal_brdf(Color::new(0.8, 0.6, 0.2));
 
-    world.add(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, diffuse_grey_material.clone()));
-    world.add(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, diffuse_red_material.clone()));
-
+    world.add(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, ground_material));
+    world.add(Sphere::new(Vec3::new(0.0, 0.0, -1.2), 0.5, center_material));
+    world.add(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, left_material));
+    world.add(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, right_material));
     let camera = CameraBuilder::new()
+        .set_samples_per_pixel(100)
+        .set_max_bounces(50)
         .set_aspect_ratio(16.0/9.0)
-        .set_image_width(1920)
-        .set_samples_per_pixel(500)
-        .set_max_bounces(32)
+        .set_image_width(400)
         .to_camera();
 
     camera.render(&world);
